907405bf97ee7865d03822c74d300c02bd8caee9
angshelino <angelinos257@gmail.com>
2025-12-31 11:39:21 -0500

    pie de pagina con logo en los comprobantes

--- app/CoreFacturalo/Templates/pdf/default/partials/footer_legend.blade.php
+++ app/CoreFacturalo/Templates/pdf/default/partials/footer_legend.blade.php
@@ -9,6 +9,24 @@
     <tr>
         <td class="text-center font-bold">Bienes transferidos y/o servicios prestados en la Amazonía para ser consumidos en la misma</td>
     </tr>
-    <br><br>
+    <br>
+</table>
+<table class="full-width">
+    <tr>
+        <td class="text-center">
+            <table cellpadding="0" cellspacing="0" style="border: 0;">
+                <tr>
+                    <td style="border: 0; padding: 0; vertical-align: middle; white-space: nowrap;">
+                        <span style="font-size: 12px; font-weight: bold;">Elaborado por tukifac.pe</span>
+                    </td>
+                    <td style="border: 0; padding: 0; padding-left: 5px; vertical-align: middle;">
+                        <a href="https://tukifac.pe" target="_blank">
+                            <img src="/storage/logo.png" width="70" height="10" />
+                        </a>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
 </table>
 </body>
