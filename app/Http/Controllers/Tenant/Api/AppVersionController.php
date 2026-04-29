07c8faf8ce9fec9f349c48f8436dd0b7709095ca
angshelino <angelinos257@gmail.com>
2026-04-29 18:26:41 -0500

    nueva versio ntukifacapp

--- app/Http/Controllers/Tenant/Api/AppVersionController.php
+++ app/Http/Controllers/Tenant/Api/AppVersionController.php
@@ -10,15 +10,15 @@ class AppVersionController extends Controller
     {
         return response()->json([
             "android" => [
-                "min_version" => "1.1.8",
-                "latest_version" => "1.1.9",
+                "min_version" => "1.1.9",
+                "latest_version" => "1.2.0",
                 "store_url" => "https://play.google.com/store/apps/details?id=com.tukifacapp",
                 "release_notes" => "Mejoras de seguridad y nuevas funciones. Es necesario actualizar."
             ],
             "windows" => [
-                "min_version" => "1.1.8",
-                "latest_version" => "1.1.9",
-                "download_url" => "https://drive.google.com/file/d/1eF5_aIlhZnYbl0BCOwqC4D_BHQOJ6oDE/view?usp=drive_link",
+                "min_version" => "1.1.9",
+                "latest_version" => "1.2.0",
+                "download_url" => "https://drive.google.com/file/d/1he2xl3hlYE71FNGyVl99SmPlw_v1Z9Di/view?usp=drive_link",
                 "release_notes" => "Correcciones críticas para Windows y nuevas funciones de impresión."
             ]
         ]);
